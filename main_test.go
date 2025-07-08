60585c3b1902acfca1b831c7155271785a054b13
Oliver Hamilton <Oliver.Hamilton@softwire.com>
2025-07-08 09:48:15 +0100

    First commit

---
+++ main_test.go
@@ -0,0 +1,20 @@
+package main
+
+import "testing"
+
+var tests = []struct {
+	name     string
+	expected string
+}{
+	{"Alice", "Hello, Alice"},
+	{"Bob", "Hello, Bob"},
+}
+
+func TestGreeting(t *testing.T) {
+	for _, test := range tests {
+		result := greeting(test.name)
+		if result != test.expected {
+			t.Errorf("incorrect greeting, for name %s got: %s expected: %s", test.name, result, test.expected)
+		}
+	}
+}
